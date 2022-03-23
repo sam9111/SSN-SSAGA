8c5197d2a30ea2ed680a9530285335447d7f3231
srivarsha13 <75779560+srivarsha13@users.noreply.github.com>
2022-03-23 10:28:56 +0530

    added footer

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,5 +1,5 @@
 import { Head, BlitzLayout } from "blitz"
-import { Box, Container } from "@chakra-ui/react"
+import { Box, Container, Center, Text } from "@chakra-ui/react"
 const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
   return (
     <>
@@ -8,6 +8,20 @@ const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Box>{children}</Box>
+      <Box>
+        <Center bg="black" pt="8px" pb="1px">
+          <Text fontSize="md" fontWeight="bold" color="white">
+            SSN SSAGA
+          </Text>
+        </Center>
+      </Box>
+      <Box>
+        <Center bg="black" pt="1px" pb="8px">
+          <Text fontSize="md" fontWeight="bold" color="white">
+            Orthotic Leg Assessment
+          </Text>
+        </Center>
+      </Box>
     </>
   )
 }
