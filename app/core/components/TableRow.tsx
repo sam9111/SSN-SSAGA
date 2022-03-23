5846064e150df85266fb5971cea99c2e1011e3f8
srivarsha13 <75779560+srivarsha13@users.noreply.github.com>
2022-03-23 14:18:14 +0530

    changed alignment of patient table

--- app/core/components/TableRow.tsx
+++ app/core/components/TableRow.tsx
@@ -18,39 +18,32 @@ function TableRow(props) {
   return (
     <Tr>
       <Td pl="0px">
-        <Flex alignItems="center" py=".8rem" minWidth="100%" flexWrap="nowrap">
-          {type == "patients" && <Avatar h={"24px"} w={"24px"} me="18px" />}
-          <Text fontSize="md" fontWeight="bold" minWidth="100%">
+        <Flex alignItems="center" minWidth="100%" flexWrap="nowrap" py=".5rem">
+          {type == "patients" && <Avatar h={"24px"} w={"24px"} me="20px" />}
+          <Text fontSize="md" fontWeight="bold" me="80px">
             {name}
           </Text>
+          <Text fontSize="md" fontWeight="bold" me="80px">
+            {status} Severity
+          </Text>
+          <Flex direction="column" me="50px">
+            <Text
+              fontSize="md"
+              color={status === "High" ? "red" : "green"}
+              fontWeight="bold"
+              py=".5rem"
+            >{`${progression}%`}</Text>
+            <Progress
+              colorScheme={status === "High" ? "red" : "green"}
+              size="xs"
+              value={progression}
+              borderRadius="15px"
+            />
+          </Flex>
+          <Button p="0px" bg="transparent">
+            <Icon as={FaEllipsisV} color="gray.400" cursor="pointer" />
+          </Button>
         </Flex>
-      </Td>
-
-      <Td>
-        <Text fontSize="md" fontWeight="bold" pb=".5rem">
-          {status} Severity
-        </Text>
-      </Td>
-      <Td>
-        <Flex direction="column">
-          <Text
-            fontSize="md"
-            color={status === "High" ? "red" : "green"}
-            fontWeight="bold"
-            pb=".2rem"
-          >{`${progression}%`}</Text>
-          <Progress
-            colorScheme={status === "High" ? "red" : "green"}
-            size="xs"
-            value={progression}
-            borderRadius="15px"
-          />
-        </Flex>
-      </Td>
-      <Td>
-        <Button p="0px" bg="transparent">
-          <Icon as={FaEllipsisV} color="gray.400" cursor="pointer" />
-        </Button>
       </Td>
     </Tr>
   )
