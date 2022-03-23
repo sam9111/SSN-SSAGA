4d0ac57ef4731ed09344ae8a4b07278e2ae38479
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-03-23 09:12:23 +0530

    add attributes

--- app/patients/mutations/createPatient.ts
+++ app/patients/mutations/createPatient.ts
@@ -4,6 +4,11 @@ import { z } from "zod"
 
 const CreatePatient = z.object({
   name: z.string(),
+  status: z.string(),
+  severity_score: z.number(),
+  mobile: z.number(),
+  email: z.string(),
+  location: z.string(),
 })
 
 export default resolver.pipe(resolver.zod(CreatePatient), resolver.authorize(), async (input) => {
